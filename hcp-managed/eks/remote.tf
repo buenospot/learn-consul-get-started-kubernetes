e441d2377189cdb061a3937e816dcbc701ab6117
Mark Buensalido <mbuensalido@mac.com>
2023-03-15 13:22:29 -0700

    added back remote

--- hcp-managed/eks/remote.tf
+++ hcp-managed/eks/remote.tf
@@ -1,10 +1,10 @@
-# terraform {
-#   cloud {
-#     organization = "buensalido"
-#     hostname = "app.terraform.io" # Optional; defaults to app.terraform.io
+terraform {
+  cloud {
+    organization = "buensalido"
+    hostname = "app.terraform.io" # Optional; defaults to app.terraform.io
 
-#     workspaces {
-#       name = "buensalido-consul-demo"
-#     }
-#   }
-# }
+    workspaces {
+      name = "buensalido-consul-demo"
+    }
+  }
+}
