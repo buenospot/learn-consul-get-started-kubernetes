fa60b2e7254400cb186489336e74caa131af6010
Mark Buensalido <mbuensalido@mac.com>
2023-03-15 13:30:16 -0700

    consul 1.15.1 - api 0.5.2

--- self-managed/eks/variables.tf
+++ self-managed/eks/variables.tf
@@ -13,13 +13,13 @@ variable "vpc_region" {
 variable "consul_version" {
   type        = string
   description = "The Consul version"
-  default     = "v1.14.4"
+  default     = "v1.15.1"
 }
 
 variable "api_gateway_version" {
   type        = string
   description = "The Consul API gateway CRD version to use"
-  default     = "0.5.1"
+  default     = "0.5.2"
 }
 
 resource "random_string" "suffix" {
