279da0f99fcfef01a83fea76fde8d42369c99e52
Mark Buensalido <mbuensalido@mac.com>
2023-03-15 13:26:39 -0700

    adding remote to self managed consul

---
+++ self-managed/eks/remote.tf
@@ -0,0 +1,10 @@
+terraform {
+  cloud {
+    organization = "buensalido"
+    hostname = "app.terraform.io" # Optional; defaults to app.terraform.io
+
+    workspaces {
+      name = "buensalido-consul-demo"
+    }
+  }
+}
